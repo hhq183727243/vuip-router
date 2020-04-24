3754d068297a6dd05c019a65d40732200c364a19
heqing.huang <heqing.huang@ucarinc.com>
2020-04-24 09:23:07 +0800

    init

---
+++ config/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const plugins = require('./webpack.config.plugins');
+
+module.exports = function (env = {}, argv) {
+    return {
+        entry: {
+            "vuip-router": path.resolve(__dirname, '../src/index.js'),
+        },
+        mode: 'production',
+        output: {
+            // 把所有依赖的模块合并输出到一个 bundle.js 文件
+            filename: '[name].min.js',
+            // 输出文件都放到 dist 目录下
+            path: path.resolve(__dirname, '../dist'),
+            library: 'vuip-router',
+            libraryTarget: 'commonjs2',
+            chunkFilename: "[id].chunk.js" // 非入口文件命名规则
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+                }, {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    loader: "ts-loader",
+                    options: {
+                        transpileOnly: true // 是否只编译，true只编译不检测，false编译检测
+                    }
+                }
+            ]
+        },
+        plugins: plugins,
+        resolve: {
+            extensions: ['.ts', '.js'],
+            alias: {
+                '@': path.resolve(__dirname, './src/'),
+            }
+        }
+    }
+};
